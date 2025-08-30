4aeaf9925851caa04a4b4d054ad83de43d24a5a3
Graham Preston <graham.preston@gmail.com>
2025-08-30 14:24:38 +0200

    Checkpoint: Write data to playable file from websocket stream

--- src/spectate/websocket_connection.rs
+++ src/spectate/websocket_connection.rs
@@ -1,26 +1,29 @@
-use futures::channel::mpsc::Receiver;
-use futures::StreamExt;
-// use tungstenite::http::{Method, Request};
+use std::pin::Pin;
+
 use tokio_tungstenite::tungstenite::client::IntoClientRequest;
 use tokio_tungstenite::connect_async;
+use async_stream::stream;
+use tokio_tungstenite::tungstenite::Message;
 
 use crate::common::SlippiDataStream;
 
 // TODO: disconnect between socketaddr and &str between here and broadcast
 // Does it matter? These functions shouldn't really be interchangable anyways,
 // maybe just the return type matters. In that case, change name too.
-pub async fn data_stream(address: &str, interrupt_receiver: Receiver<bool>) { // -> Pin<Box<SlippiDataStream>> {
-  tracing::info!("Connecting to SpectatorMode...");
-  let request = address.into_client_request().unwrap();
-  let (stream, response) = connect_async(request).await.unwrap();
-  println!("connect response {:?}", response);
-  tracing::info!("Connected to SpectatorMode.");
+pub async fn data_stream(address: &str) -> Pin<Box<SlippiDataStream>> {
+    tracing::info!("Connecting to SpectatorMode...");
+    let request = address.into_client_request().unwrap();
+    let (stream, response) = connect_async(request).await.unwrap();
+    println!("connect response {:?}", response);
+    tracing::info!("Connected to SpectatorMode.");
 
-  stream.map(|v| {
-    println!("got message v {:?}", v);
-  }).collect::<()>().await;
-  
-  // Box::pin(stream! {
-  //   yield Vec::new();
-  // })
+    Box::pin(stream! {
+        for await result in stream {
+            if let Ok(Message::Binary(bytes)) = result {
+                yield bytes.to_vec();
+            } else {
+                break;
+            }
+        }
+    })
 }
