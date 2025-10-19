5accb47260642e4aa1cb0c811c41119dc9f9b2e8
Graham Preston <graham.preston@gmail.com>
2025-10-19 13:31:49 +0200

    Add .exe

--- scripts/cmd/broadcast.cmd
+++ scripts/cmd/broadcast.cmd
@@ -1,3 +1,3 @@
 &commat;echo off
 SET script_dir="%~dp0"
-%script_dir%\swb broadcast
+%script_dir%\swb.exe broadcast
--- scripts/cmd/spectate.cmd
+++ scripts/cmd/spectate.cmd
@@ -1,4 +1,4 @@
 &commat;echo off
 SET script_dir="%~dp0"
 set /p "streamid=Enter the stream ID to spectate: "
-%script_dir%/swb spectate %streamid%
+%script_dir%/swb.exe spectate %streamid%
